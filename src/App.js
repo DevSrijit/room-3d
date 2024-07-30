26cd5e79d7749a8c8446217a3694f0d3a528378f
devsrijit <srijitghosh7331@gmail.com>
2024-07-30 23:39:23 +0530

    chore: Update spotLight color to pink in App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import { Room } from './Room'
 
 export default function App() {
   return (
+    <>
     <Canvas orthographic shadows dpr={[1, 2]} camera={{ position: [10, 10, 10], zoom: 10 }}>
       <color attach="background" args={['#252530']} />
       <ambientLight intensity={0.01} />
       <hemisphereLight intensity={0.125} color="#8040df" groundColor="red" />
-      <spotLight castShadow color="orange" intensity={2} position={[-50, 50, 40]} angle={0.25} penumbra={1} shadow-mapSize={[128, 128]} shadow-bias={0.00005} />
+      <spotLight castShadow color="pink" intensity={2} position={[-50, 50, 40]} angle={0.25} penumbra={1} shadow-mapSize={[128, 128]} shadow-bias={0.00005} />
 
       <Bounds fit clip observe margin={1}>
         <Room scale={0.1} />
@@ -31,5 +32,6 @@ export default function App() {
         <shadowMaterial transparent opacity={0.3} />
       </mesh>
     </Canvas>
+    </>
   )
 }
